db7e70b44ffef09f2bc7f63c2bb8bbcbd80f06cf
Kathleen Ashley <81791184+srslyashh@users.noreply.github.com>
2022-12-14 13:33:05 -0800

    Update init.r

--- init.r
+++ init.r
@@ -4,13 +4,14 @@
 #
 
 
-
+# Listing the packages that would be needed for the app to run
 my_packages = c( "sp", "mapview", "rgdal","shiny", "shiny.router", 
                 "tidyverse", "shinythemes", "shinyjs", "htmlwidgets", "leaflet", "tigris", "raster",
                  "plyr", "dplyr", "plotly", "fresh")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
+    # Downloads the dependencies that each package would need along with the packages
     install.packages(p, dependencies=TRUE)
   }
 }
